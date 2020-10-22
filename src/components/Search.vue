c3a36762520324f669fff92c9f3f06ddb39bc49b
daniel-lopes <daniellopesdasilva98@gmail.com>
2020-10-21 23:05:22 -0300

    Starting the Home

---
+++ src/components/Search.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Github <i>Search</i></h1>
+    <input type="text" v-model="username">  
+    <button @click="search()">Pesquisar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  data() {
+    return {
+      username: '',
+      userData: []
+    }
+  },
+  methods: {
+    search: function(){
+      fetch(`https://api.github.com/users/${this.username}`)
+      .then(response => response.json())
+      .then(result => {
+        this.userData = result;
+        console.log("retorno => " + this.userData);
+      })
+
+    }
+  },  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/UserData.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Dados do Usuário</h1>
+  </div>
+</template>
